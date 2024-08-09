14f330457a11ae3372b6e2ff95e99326c9420b0e
Hsupei <hsupei0514@gmail.com>
2024-08-09 23:52:10 +0800

    Add ExerciseRouter and Userrouter

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from dbconfig import engine, Base
+from router.exercise import ExerciseRouter
+from router.user import Userrouter
 
-app = FastAPI()
+# 創建所有 table，先執行檢查
+Base.metadata.create_all(bind=engine, checkfirst=True)
+
+app = FastAPI(debug=True)
 
 app.mount("/static", StaticFiles(directory="static", html=True))
 
@@ -25,3 +31,6 @@ async def index():
 @app.get("/diet")
 async def index():
     return FileResponse("./static/html/diet.html", media_type="text/html")
+
+app.include_router(ExerciseRouter)
+app.include_router(Userrouter)
