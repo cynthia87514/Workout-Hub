820e1d028bc031110cb6a92ba27a503fdff90225
Hsupei <hsupei0514@gmail.com>
2024-08-12 22:34:04 +0800

    Add logic of detecting local time zone

--- model/user.py
+++ model/user.py
@@ -1,10 +1,23 @@
-from sqlalchemy import Column, Integer, String
+from datetime import datetime
+from pytz import timezone
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
 from dbconfig import Base
+
+# 定義台北時區
+taipei_tz = timezone("Asia/Taipei")
+
+def get_taipei_time():
+    return datetime.now(taipei_tz)
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True)
-    email = Column(String(100), unique=True, index=True)
-    hashed_password = Column(String(100))
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    username = Column(String(255), nullable=False)
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    hashed_password = Column(String(255), nullable=False)
+    created_at = Column(DateTime, default=get_taipei_time)
+    updated_at = Column(DateTime, default=get_taipei_time, onupdate=get_taipei_time)
+    
+    workouts = relationship("Workouts", back_populates="user", cascade="all, delete-orphan")
